aa0b30764f743b390fde8b8b46595ed6e239eac7
Foobar Q <joshua6225@hotmail.com>
2021-06-28 00:43:18 +0100

    fix build

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -24,8 +24,8 @@
         >@UKPetitionPosts</a
       >. The twitter bot's purpose is to show people more of what's going on in
       government - the stuff that isn't usually covered in the daily news
-      briefings, whereas the purpose of this site is to show just how effective -
-      or ineffective the petition process is with bringing about real change.
+      briefings, whereas the purpose of this site is to show just how effective
+      - or ineffective the petition process is with bringing about real change.
       The website is still being fleshed out but for now: please click around to
       see what you can see, and follow @UKPetitionPosts on twitter to send
       feedback on the site and/or to get petition updates right in your feed.
