002dd21f66f3f9858c13f2d287f12535d1c5fde1
Joshua Hall <43988692+FoobarQ@users.noreply.github.com>
2022-09-09 12:53:49 +0100

    added: delete script to help manage database size

---
+++ delete.ts
@@ -0,0 +1,44 @@
+import "reflect-metadata";
+import pg from "pg";
+
+const client = new pg.Pool({
+    user: process.env.TIMESCALE_USER || "user",
+    host: process.env.TIMESCALE_HOST || "host",
+    database: process.env.TIMESCALE_DB || "",
+    password: process.env.TIMESCALE_PASSWORD || "",
+    port: parseInt(process.env.TIMESCALE_PORT || "0"),
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+async function getExpiredPetitionIds(): Promise<string[]> {
+    const result = await client.query(
+        "SELECT id FROM petition WHERE response = TRUE AND debate = TRUE"
+    );
+
+    return result.rows.map(row => row.id);
+}
+
+async function getPetitionsWithResponseIds(): Promise<string[]> {
+    const result = await client.query(
+        "SELECT id FROM petition WHERE deadline < now()"
+    );
+
+    return result.rows.map(row => row.id);
+}
+
+async function deletePetitionsById(ids: string[]) {
+    await client.query(
+        "DELETE FROM petition WHERE id = ANY($1)",
+        ids
+    );
+
+    console.log(`Deleted ${ids.length} petitions`);
+    return ids.length;
+}
+
+getExpiredPetitionIds()
+    .then((ids) => deletePetitionsById(ids))
+    .then(() => getPetitionsWithResponseIds())
+    .then((ids) => deletePetitionsById(ids))
